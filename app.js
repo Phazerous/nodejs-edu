5aafe51ca2e117902b76c7419cce1c209ad2cc6b
Phazerous <nphazerous@gmail.com>
2023-01-28 01:04:34 +0600

    body-parser middleware

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const users = require('./data');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/user', (req, res, next) => {
   console.log('Someone sent a request!');
@@ -10,6 +14,19 @@ app.use('/user', (req, res, next) => {
 
 app.use('/users', (req, res, next) => {
   res.send(users);
+});
+
+app.use('/register', (req, res, next) => {
+  res.send(`
+    <form action="/create-user" method="POST">
+      <input type="text" name="username" placeholder="Username" />
+      <button type="submit">Register</button>
+    </form>
+  `);
+});
+
+app.use('/create-user', (req, res, next) => {
+  console.log(req.body);
 });
 
 app.use('/', (req, res, next) => {
