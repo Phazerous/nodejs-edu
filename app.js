32a38b95f2935f7ff578510672a9bf0f26364972
Phazerous <nphazerous@gmail.com>
2023-01-28 22:28:29 +0600

    Static file serving

--- app.js
+++ app.js
@@ -3,17 +3,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
+app.use(express.static(path.join(__dirname, 'public')));
 
-const adminRouter = require('./routes/admin');
-// const shopRouter = require('./routes/shop');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/admin', adminRouter);
-
-app.use('/', (req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'views', 'home-page.html'));
 });
 
 app.listen(3000);
