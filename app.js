c722636e871b54fef5e4c6f788009243dda7604a
Phazerous <nphazerous@gmail.com>
2023-01-29 22:35:47 +0600

    Basic PUG

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const path = require('path');
-
 const express = require('express');
 const app = express();
 
 app.set('view engine', 'pug');
+app.use(express.static('public'));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res, next) => {
-  res.render('shop');
+app.get('/home', (req, res, next) => {
+  res.render('home-page.pug', { pageTitle: 'Home page', path: '/home' });
 });
 
 app.listen(3000);
