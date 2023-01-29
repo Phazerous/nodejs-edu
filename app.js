3996785e0c244599299e0dee7c48e15a0714c14b
Phazerous <nphazerous@gmail.com>
2023-01-29 21:53:20 +0600

    View Engines in Express + rendering PUG

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'views', 'home-page.html'));
+  res.render('shop');
 });
 
 app.listen(3000);
